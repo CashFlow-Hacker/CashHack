e32289837d67b8f7710eb1bc7592a44ba69de144
CashFlow-Hacker <55640189+CashFlow-Hacker@users.noreply.github.com>
2019-11-12 15:41:27 -0500

    TetsResolverShit
    T

---
+++ Features/Resolver/TestResolver.cpp
@@ -0,0 +1,7 @@
+if (entity->GetVelocity().Length2D() = 0.f)
+switch (Test)
+{
+  case 1: animstate->m_flEyeYaw == -60 : 60; break;
+  case 2: animstate->m_flEyeYaw == -120: 120; break;
+  case 3: animstate->m_flEyeYaw == 0; break;
+ }
